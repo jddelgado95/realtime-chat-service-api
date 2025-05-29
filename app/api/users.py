a08b0fe91907643e288710230abc7525a39ab74d
jddelgado95 <jddelgado95@gmail.com>
2025-05-29 17:53:15 -0600

    Added users logic to handle api

---
+++ app/api/users.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app import models, schemas
+from app.db.session import get_db
+
+router = APIRouter()
+
+@router.get("/{user_id}", response_model=schemas.user.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.user.User).filter(models.user.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
