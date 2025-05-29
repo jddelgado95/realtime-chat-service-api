7798d58b6ea7acf141824e78182b761324c5a8d3
jddelgado95 <jddelgado95@gmail.com>
2025-05-29 17:38:19 -0600

    Added auth logic for the api

---
+++ app/api/auth.py
@@ -0,0 +1,10 @@
+from fastapi.security import OAuth2PasswordRequestForm
+
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.user.User).filter_by(email=form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    token = create_access_token({"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
