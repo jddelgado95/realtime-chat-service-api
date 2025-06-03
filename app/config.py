18c0becbb54197122d93e546842fd27664f1e46f
jddelgado95 <jddelgado95@gmail.com>
2025-06-02 19:24:17 -0600

    Added config logic

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    REDIS_URL: str = "redis://redis:6379/0"
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
